1544c9c61198ecdcd217ee2a85cf8f6a192faba4
davidaknowles <knowles84@gmail.com>
2015-12-04 13:08:50 -0800

    get numerators in RData format

---
+++ clustering/convert_to_RData.R
@@ -0,0 +1,10 @@
+
+args=commandArgs(trailingOnly = T)
+
+numers=read.table(args[1], header=T, row.names=1, stringsAsFactors=F)
+
+#ss=strsplit(colnames(numers),split="_")
+#meta=do.call(rbind,ss)
+#colnames(meta)=c("sex","tissue","id")
+
+save(numers, file=args[2] ) 
