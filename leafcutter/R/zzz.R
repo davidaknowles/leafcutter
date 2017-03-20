e47d8ee7ff017eee1c8cbb6c8538493d7e32af1e
David Knowles <knowles84@gmail.com>
2017-03-20 15:55:12 -0700

    Updated loadRcppModules to loadModule

--- leafcutter/R/zzz.R
+++ leafcutter/R/zzz.R
@@ -1,2 +1,7 @@
-.onLoad <- function(libname, pkgname) { Rcpp::loadRcppModules() }
+.onLoad <- function(libname, pkgname) {
+  modules <- paste0("stan_fit4", names(stanmodels), "_mod")
+  for (m in modules) loadModule(m, what = TRUE)
+}
+
+# .onLoad <- function(libname, pkgname) { Rcpp::loadRcppModules() }
 
