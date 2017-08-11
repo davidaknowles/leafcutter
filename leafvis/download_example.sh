48ace23fbefeb89209be79022c6f1292c3c47977
David Knowles <knowles84@gmail.com>
2017-08-11 15:20:34 -0700

    Rename leafvis to leafviz

--- leafvis/download_example.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-curl http://web.stanford.edu/~dak33/leafvis_example.zip -o example.zip
-unzip example.zip
---
+++ leafviz/download_example.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+curl http://web.stanford.edu/~dak33/leafviz_example.zip -o example.zip
+unzip example.zip
