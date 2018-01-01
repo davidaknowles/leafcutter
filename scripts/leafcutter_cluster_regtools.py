9e94d5c6217929c7e8ee22fe5ad18ce067baa1a2
Yang Li <yangili1@midway2-login1.rcc.local>
2018-01-01 15:28:58 -0600

    added some commands in the scripts header

--- scripts/leafcutter_cluster_regtools.py
+++ scripts/leafcutter_cluster_regtools.py
@@ -1,3 +1,7 @@
+# https://github.com/griffithlab/regtools
+# /home/yangili1/tools/regtools/build/regtools junctions extract -a 8 -i 50 -I 500000 bamfile.bam -o outfile.junc
+# Using regtools speeds up the junction extraction step by an order of magnitude or more
+
 import sys
 import tempfile
 import os
