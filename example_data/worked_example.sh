f90d2bff4729583b2445e7f23258b6e24323901d
David A. Knowles <knowles84@gmail.com>
2018-10-22 12:18:00 -0700

    issue68 fix

--- example_data/worked_example.sh
+++ example_data/worked_example.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Manually working through the differential splicing pipeline. 
+# Manually working through the differential splicing pipeline.
 
 # wget bam files (4Gb!)
 wget https://www.dropbox.com/s/8mxmkoa09p24k1g/example_geuvadis.tar.gz?dl=0 -O example_geuvadis.tar.gz
@@ -15,11 +15,26 @@ for bamfile in `ls example_geuvadis/*chr1.bam`; do
     echo $bamfile.junc >> test_juncfiles.txt
 done
 
-# Finds intron clusters and quantifies junction usage within them. 
+# Finds intron clusters and quantifies junction usage within them.
 python ../clustering/leafcutter_cluster.py -j test_juncfiles.txt -m 50 -o testYRIvsEU -l 500000
 
-# Differential splicing analysis. 
+# Differential splicing analysis.
 ../scripts/leafcutter_ds.R --num_threads 4 ../example_data/testYRIvsEU_perind_numers.counts.gz example_geuvadis/groups_file.txt
 
-# Plot differentially spliced clusters.
+# Plot pdf differentially spliced clusters.
 ../scripts/ds_plots.R -e ../leafcutter/data/gencode19_exons.txt.gz ../example_data/testYRIvsEU_perind_numers.counts.gz example_geuvadis/groups_file.txt leafcutter_ds_cluster_significance.txt -f 0.05
+
+## Leafviz visualization shiny app
+pushd ../leafviz/
+# Download hg19 annotation files
+./download_human_annotation_codes.sh
+# Cache results for viewing
+./prepare_results.R --meta_data_file ../example_data/example_geuvadis/groups_file.txt \
+  --code leafcutter ../example_data/testYRIvsEU_perind_numers.counts.gz \
+  ../example_data/leafcutter_ds_cluster_significance.txt \
+  ../example_data/leafcutter_ds_effect_sizes.txt \
+  annotation_codes/gencode_hg19/gencode_hg19 \
+  -o testYRIvsEU.RData
+# Launch shiny app locally
+./run_leafviz.R testYRIvsEU.RData
+popd
--- leafviz/download_human_annotation_codes.sh
+++ leafviz/download_human_annotation_codes.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 curl http://web.stanford.edu/~dak33/leafcutter/annotation_codes.zip -o annotation_codes.zip
-unzip annotation_codes.zip
+unzip -f annotation_codes.zip
