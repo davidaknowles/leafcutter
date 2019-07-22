9bbfb49faf07a554c9bd803bf29fc5f877d98ca2
Horacio Montenegro <h.montenegro@gmail.com>
2019-07-22 15:10:13 -0300

    Fix empty gene_name at gtf_to_exons.R, using gene_id when necessary. Move all additional code to an if() block.

--- scripts/gtf_to_exons.R
+++ scripts/gtf_to_exons.R
@@ -18,7 +18,18 @@ gtf = gtf %>% filter( feature=="exon" )
 gn_where=regexpr("gene_name \"[^ ]+\"" , gtf$dat) # find gene_names in dat
 gn_where=gn_where + 11 # ignore "gene_name" label
 attr(gn_where,"match.length")=attr(gn_where,"match.length") - 11- 1 # cutoff trailing quote mark
+
 gtf$gene_name=regmatches(gtf$dat, gn_where ) 
+
+if( any( gtf$gene_name== "" ) ){ 
+  cat("Warning: there are empty 'gene_name' attributes, using 'gene_id' for them\n")
+  gi_where=regexpr("gene_id \"[^ ]+\"" , gtf$dat) # find gene_ids in dat
+  gi_where=gi_where + 9 # ignore "gene_id" label
+  attr(gi_where,"match.length")=attr(gi_where,"match.length") - 9- 1 # cutoff trailing quote mark
+  gtf$gene_id=regmatches(gtf$dat, gi_where )
+  gtf$gene_name[ gtf$gene_name == "" ] <- gtf$gene_id[ gtf$gene_name == "" ]
+  gtf=select (gtf,-gene_id )
+}
 
 #gtf$gene=foreach(s=strsplit(gtf$dat," "), .combine=c) %dopar% { s[which(s=="gene_name")+1] }
 #gtf$gene=substr(gtf$gene, 1, nchar(gtf$gene)-1)
