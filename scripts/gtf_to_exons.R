717d0ed849f93311588c271f436d2725e2cd7388
davidaknowles <knowles84@gmail.com>
2019-08-14 14:57:19 -0400

    fread security fix

--- scripts/gtf_to_exons.R
+++ scripts/gtf_to_exons.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-require(dplyr) 
+require(dplyr)
 require(data.table)
 
 args=commandArgs(trailingOnly = T)
@@ -10,7 +10,7 @@ if (length(args)<2) {
 }
 
 cat("Reading in ",args[1],"\n")
-gtf=fread(paste("zcat <", args[1]), data.table = F, col.names=c("chr","source","feature","start","end","a","strand","b","dat"))
+gtf=fread(cmd=paste("zcat <", args[1]), data.table = F, col.names=c("chr","source","feature","start","end","a","strand","b","dat"))
 
 cat("Processing...\n")
 gtf = gtf %>% filter( feature=="exon" )
@@ -19,9 +19,9 @@ gn_where=regexpr("gene_name \"[^ ]+\"" , gtf$dat) # find gene_names in dat
 gn_where=gn_where + 11 # ignore "gene_name" label
 attr(gn_where,"match.length")=attr(gn_where,"match.length") - 11- 1 # cutoff trailing quote mark
 
-gtf$gene_name=regmatches(gtf$dat, gn_where ) 
+gtf$gene_name=regmatches(gtf$dat, gn_where )
 
-if( any( gtf$gene_name== "" ) ){ 
+if( any( gtf$gene_name== "" ) ){
   cat("Warning: there are empty 'gene_name' attributes, using 'gene_id' for them\n")
   gi_where=regexpr("gene_id \"[^ ]+\"" , gtf$dat) # find gene_ids in dat
   gi_where=gi_where + 9 # ignore "gene_id" label
